b2757bb65844b8267350aff638cb865c6745bbe4
Victor Kubankov <syslin51239@gmail.com>
2025-11-11 16:02:41 +0300

    Изменена основная логика добавлена коробка

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,8 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("11111");
+        System.out.println("10001");
+        System.out.println("22222");
+        System.out.println("33333");
     }
 }
